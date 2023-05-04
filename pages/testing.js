bfdb2d598ee70c7149fc89a5f75a5a554f2ccc71
isaiah749 <isaiah749@gmail.com>
2023-05-04 15:09:11 -0400

    added snapping and a total of 5 prices

--- pages/testing.js
+++ pages/testing.js
@@ -5,17 +5,17 @@ import JunoPlus from '../components/Testing/JunoPlus'
 
 function Testing() {
 
-    const [data, setData] = useState(99)
+    const [data, setData] = useState(3)
     const [content, setContent] = useState(<Juno />)
 
     useEffect(() => {
-      if (data < 490) {
+      if (data < 3) {
         setContent(<Juno />)
       }
-      else if (data >= 490 && data <= 899) {
+      else if (data == 3 && data < 5 ) {
         setContent(<JunoPlus />)
       }
-      else if (data == 900) {
+      else if (data == 5) {
         setContent(<JunoAgency />)
       }
       else {
@@ -29,16 +29,24 @@ function Testing() {
            {content}
         </section>
         <div className='w-full max-w-7xl mx-auto flex items-center flex-col'>
-            <input className='lg:w-[500px] lg:h-3 bg-gray-200 rounded-lg  cursor-pointer lg:range-lg  w-[350px] h-2 mb-3  accent-green-500 ' type='range' min='99' max='900' step='1' value={data} onChange={(e) => setData(e.target.value)}  />
+            <input className='lg:w-[500px] lg:h-3 bg-gray-200 rounded-lg  cursor-pointer lg:range-lg  w-[350px] h-2 mb-3  accent-green-500 ' type='range' min='1' max='5' step='1' value={data} onChange={(e) => setData(e.target.value)}  />
             <h1 className='font-bold text-green-500 text-3xl'>
-                {`$${data}/Month`}
-                <span className={data < 900 ? 'block' : 'hidden'}>
-                  {data <= 490 ? (<div>$99/Month</div>) : (<div>$490/Month</div>)}
+                {/* {`$${data}/Month`} */}
+                <span className={data == 1 ? 'block' : 'hidden'}>
+                  <div>$99/Month</div>
                 </span>
-                <span className={data >= 900 ? 'block' : 'hidden'}>
+                <span className={data == 2 ? 'block' : 'hidden'}>
+                  <div>$250/Month</div>
+                </span>
+                <span className={data == 3 ? 'block' : 'hidden'}>
+                  <div>$490/Month</div>
+                </span>
+                <span className={data == 4 ? 'block' : 'hidden'}>
+                  <div>$700/Month</div>
+                </span>
+                <span className={data == 5 ? 'block' : 'hidden'}>
                   <div>$900/Month</div>
                 </span>
-                
             </h1>
         </div>
     </div>
